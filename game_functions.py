432721f03f07d280f09cbb64436d6eef8518da1a
egor <egorkam2005@gmail.com>
2020-03-02 21:03:03 +0800

    Add aliens

--- game_functions.py
+++ game_functions.py
@@ -14,6 +14,8 @@ def check_keydown_events (event, ai_settings, screen, ship, bullets):
         ship.moving_down = True
     elif event.key == pygame.K_SPACE:
         fire_bullet (ai_settings, screen, ship, bullets)
+    elif event.key == pygame.K_q:
+        sys.exit()
 def check_keyup_events (event, ship):
     if event.key == pygame.K_RIGHT:
         ship.moving_right = False
@@ -85,14 +87,24 @@ def change_fleet_direction(ai_settings, aliens):
         alien.rect.y += ai_settings.fleet_drop_speed
     ai_settings.fleet_direction *= -1
 def ship_hit(ai_settings, stats,screen,ship,aliens,bullets):
-    stats.ship_left -= 1
-    aliens.empty()
-    bullets.empty()
-    create_fleet(ai_settings, screen, ship, aliens)
-    ship.center_ship()
-    sleep(0.5)
+    if stats.ships_left > 0:
+        stats.ships_left -= 1
+        aliens.empty()
+        bullets.empty()
+        create_fleet(ai_settings, screen, ship, aliens)
+        ship.center_ship()
+        sleep(0.5)
+    else:
+        stats.game_active = False
+def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
+    screen_rect = screen.get_rect()
+    for alien in aliens.sprites():
+        if alien.rect.bottom >= screen_rect.bottom:
+            ship_hit(ai_settings,stats,screen,ship,aliens,bullets)
+            break
 def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
     check_fleet_edges(ai_settings, aliens)
     aliens.update()
     if pygame.sprite.spritecollideany(ship,aliens):
         ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+        check_aliens_bottom(ai_settings,stats,screen,ship,aliens,bullets)
