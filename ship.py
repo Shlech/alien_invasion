59eebd6601276f8ad7535ddc05366655a3a08fda
egor <egorkam2005@gmail.com>
2020-03-04 23:03:05 +0800

    Add aliens

--- ship.py
+++ ship.py
@@ -1,9 +1,11 @@
 import pygame
-class Ship():
+from pygame.sprite import Sprite
+class Ship(Sprite):
     def __init__(self, ai_settings, screen):
+        super(Ship, self).__init__()
         self.screen = screen
         self.ai_settings = ai_settings
-        self.image = pygame.image.load ('images/ship.png')
+        self.image = pygame.image.load ('images/ship4.png')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
         self.rect.centerx = self.screen_rect.centerx
