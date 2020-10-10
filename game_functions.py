8cdc2a4d9ed18a1c82081b1b8c347d9ddc7e3a19
egor <egorkam2005@gmail.com>
2020-10-10 20:19:32 +0800

    Корретктировка количества рядов

--- game_functions.py
+++ game_functions.py
@@ -90,7 +90,7 @@ def get_number_aliens_x(ai_settings, alien_width):
     return number_aliens_x
 def get_number_rows(ai_settings, ship_height, alien_height):
     available_space_y = (ai_settings.screen_height -(3 * alien_height) - ship_height)
-    number_rows = int(available_space_y / (2 * alien_height)) -2
+    number_rows = int(available_space_y / (2 * alien_height)) -3
     return number_rows
 def create_alien(ai_settings, screen, aliens,alien_number,row_number):
     alien = Alien(ai_settings,screen)
